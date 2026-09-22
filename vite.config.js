ce61b0e3199c2c1301128c5d8216c37ff120a891
Sushant Bhatta <sushantbhatta7@gmail.com>
2026-09-22 01:58:46 -0400

    Fix custom domain asset paths

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: process.env.GITHUB_PAGES === 'true' ? '/portfolioSushant/' : '/',
+  base: '/',
   plugins: [react(), tailwindcss()],
 })
